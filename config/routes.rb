60e631fec690493a2894622dcda059f23b52e5d0
Samy Messaoudi <samymessaoudi37@gmail.com>
2016-11-14 17:22:10 +0100

    chiffre et lettre

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'game' => 'pages#game'
+  get 'score' => 'pages#score'
+
+
+  root to: 'pages#game'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
